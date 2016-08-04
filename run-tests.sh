fcd7cc98c1c90e1106d2a693eb098d06a8f6670e
Jonathan Cottrill <github@jonathancottrill.net>
2016-08-04 09:09:52 -0500

    Add downloadVerified function, with improved GPG security and more flexibility for multiple file downloads (future runner handling)

--- run-tests.sh
+++ run-tests.sh
@@ -4,16 +4,16 @@ unset ROOT
 unset SETTINGS_FILE_NAME
 unset SETTINGS_FILE
 unset PROJECT
-unset GPG_KEY_ID
-unset GPG_KEY_UID
+unset GPG_KEY_FINGERPRINT
+unset GPG_KEY_SUBJECT
 unset RUNNER_PACKAGE_NAME
 
 ROOT=$(readlink -f "$(dirname "$0")")
 SETTINGS_FILE_NAME=testbasher-settings.json
 SETTINGS_FILE="$ROOT/$SETTINGS_FILE_NAME"
 PROJECT=perihelios/total-garbage
-GPG_KEY_ID=547B76E4C0C322E8
-GPG_KEY_UID="Perihelios LLC <pgp@perihelios.com>"
+GPG_KEY_FINGERPRINT=1F358F99E6314E45B4AAFA7F547B76E4C0C322E8
+GPG_KEY_SUBJECT="Perihelios LLC <pgp@perihelios.com>"
 RUNNER_PACKAGE_NAME=dummy.tar.gz
 
 fail() {
@@ -61,8 +61,11 @@ getSettingsVersion() {
 }
 
 ensureGpgKeyInKeystore() {
-	if ! gpg --list-keys $GPG_KEY_ID >/dev/null 2>&1; then
-		fail "ERROR: GPG key for \"$GPG_KEY_UID\" (key ID $GPG_KEY_ID) not found in keystore; did you import it?"
+	local gpgKeyFingerprint="$1"
+	local gpgKeySubject="$2"
+
+	if ! gpg --batch --no-tty --list-keys "$gpgKeyFingerprint" >/dev/null 2>&1; then
+		fail "ERROR: GPG key for \"$gpgKeySubject\" (key ID/fingerprint $gpgKeyFingerprint) not found in keystore; did you import it?"
 	fi
 }
 
@@ -127,6 +130,44 @@ download() {
 	esac
 }
 
+downloadVerified() {
+	local url="$1"
+	local localFile="$2"
+	local downloadDescription="$3"
+	local gpgKeyFingerprint="$4"
+	local gpgKeySubject="$5"
+	local signatureUrl="$url.asc"
+	local signatureLocalFile="$localFile.asc"
+
+	ensureGpgKeyInKeystore "$gpgKeyFingerprint" "$gpgKeySubject"
+
+	download "$url" "$localFile" "$downloadDescription"
+	download "$signatureUrl" "$signatureLocalFile" "$downloadDescription GPG signature"
+
+	local verifyOutput
+	verifyOutput=`gpg --batch --no-tty --status-fd 1 --verify "$signatureLocalFile" "$localFile" 2>/dev/null`
+
+	if [ $? -ne 0 ]; then
+		fail "ERROR: SECURITY! Downloaded file $localFile FAILED GPG VERIFICATION!"
+	fi
+
+	local line
+	local verifiedWithFingerprint
+
+	while IFS='' read -r line || [ -n "$line" ]; do
+		if [[ "$line" =~ VALIDSIG\ ([0-9A-F]+) ]]; then
+			verifiedWithFingerprint="${BASH_REMATCH[1]}"
+			break
+		fi
+	done <<<"$verifyOutput"
+
+	if [ ! "$verifiedWithFingerprint" == "$gpgKeyFingerprint" ]; then
+		fail "ERROR: SECURITY! Downloaded file $localFile signed with unexpected GPG key $verifiedWithFingerprint; expected $gpgKeyFingerprint"
+	fi
+
+	rm -f "$signatureLocalFile"
+}
+
 ensureRunnerPresent() {
 	local version
 	version=`getSettingsVersion` || exit $?
@@ -143,16 +184,11 @@ ensureRunnerPresent() {
 
 		local githubPrefix="https://raw.githubusercontent.com/$PROJECT/$version"
 
-		download "$githubPrefix/$RUNNER_PACKAGE_NAME" "$runnerDownloadPackage" "test runner version $version"
-		download "$githubPrefix/$RUNNER_PACKAGE_NAME.asc" "$runnerDownloadPackageSig" "GPG signature for test runner version $version"
+		downloadVerified "$githubPrefix/$RUNNER_PACKAGE_NAME" "$runnerDownloadPackage" "test runner version $version" "$GPG_KEY_FINGERPRINT" "$GPG_KEY_SUBJECT"
 
-		if gpg --verify "$runnerDownloadPackageSig" "$runnerDownloadPackage" >/dev/null 2>&1; then
-			tar -xzf "$runnerDownloadPackage" -C "$runnerDownloadDir"
-			rm "$runnerDownloadPackage" "$runnerDownloadPackageSig"
-			mv "$runnerDownloadDir" "$runnerDir"
-		else
-			fail "ERROR: SECURITY! Downloaded runner script FAILED GPG VERIFICATION!" >&2
-		fi
+		tar -xzf "$runnerDownloadPackage" -C "$runnerDownloadDir"
+		rm "$runnerDownloadPackage"
+		mv "$runnerDownloadDir" "$runnerDir"
 	fi
 }
 
